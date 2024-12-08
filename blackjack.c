74a35d778df2ff11e693503fac716b8ee9977f85
Seeb04 <haseebharoon04@gmail.com>
2024-12-08 14:51:58 +0500

    final for real

--- blackjack.c
+++ blackjack.c
@@ -8,7 +8,7 @@
 
 int deck[] = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
 int userPoints = 1000;
-
+int invalidChoice;
 
 int drawCard();//draws a card
 int calculateTotal(int cards[], int size); //calculate total score of hand
@@ -19,7 +19,24 @@ int betWin(int userBet, float multiplier);//calculate winning amount
 void winPoints(int winAmount); //updates points if win
 void losePoints(int userBet);//updates points if lose
 void clearScreen(); //to clear console
+void displayLogo(); //displays logo
 
+
+void displayLogo() {
+    const char *logo = 
+        " .------.            _     _            _    _            _     \n"
+        " |A .   |.          | |   | |          | |  (_)          | |    \n"
+        " | / \\  |-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __ \n"
+        " |(_,_) | /\\  |     | '_ \\| |/ _` |/ __| |/ / |/ _` |/ __| |/ / \n"
+        " |  I  ||  \\  |     | |_) | | (_| | (__|   <| | (_| | (__|   <  \n"
+        " `-----| \\  / |     |_.__/|_|\\__,_|\\___|_|\\_\\_|\\__,_|\\___|_|\\_\\ \n"
+        "       |  \\/ K|                            _/ |                 \n"
+        "       `------'                           |__/                  \n";
+    printf("--------------------------------------------------\n");
+    printf("\t     !!WELCOME TO BLACKJACK!!\n");
+    printf("--------------------------------------------------\n");
+    printf("%s\n", logo);
+}
 
 void clearScreen() {
 #ifdef _WIN32
@@ -28,7 +45,6 @@ void clearScreen() {
     system("clear"); // Linux/macOS
 #endif
 }
-
 
 void losePoints(int userBet) {
     printf("\nYou lost %d points!\n", userBet);
@@ -102,8 +118,10 @@ void blackjackGame() {
     int userCards[10], compCards[10]; //used to store cards of user and computer
     int userCardCount = 0, compCardCount = 0; //used to keep count for the amount of cards in hand for each player
     int userTotal = 0, compTotal = 0;
-    int userBet, invalidChoice = 0;
+    int userBet; 
     char userChoice;
+
+    invalidChoice = 0;
 
     // Initialize the random number generator
     srand(time(NULL));
@@ -120,9 +138,10 @@ void blackjackGame() {
         if (userBet < 0 || userBet > userPoints) {
             invalidChoice = 1;
             printf("\n--------------------------------------------------\n");
-            printf("Insufficient points!");
+            printf("Insufficient points!\n");
+            printf("You have have %d points!", userPoints);
             printf("\n--------------------------------------------------\n");
-            printf("\nEnter Correct bet : ");
+            printf("\nEnter bet : ");
             scanf("%d", &userBet);
         } else {
             invalidChoice = 0;
@@ -166,7 +185,7 @@ void blackjackGame() {
     do {
         //Prompting user whether they would like to hit or stand
         printf("\n--------------------------------------------------\n");
-        printf("Would you like to hit? Type 'h' or 's': ");
+        printf("Would you like to hit or stand? Type 'h' or 's': ");
         scanf(" %c", &userChoice);
         printf("--------------------------------------------------\n");
 
@@ -221,9 +240,10 @@ void blackjackGame() {
 int main() {
     char playAgain;
 
+    displayLogo();
+
     do {
         int playerLost = 0; //used for case checking
-
         blackjackGame();
 
         if (userPoints <= 0) {
@@ -232,22 +252,47 @@ int main() {
             printf("\n--------------------------------------------------\n");
             playerLost = 1;
         }
-
+        
         if (playerLost != 1) { //implies that game is not over yet
-            printf("\nWould you like to play another round? Type 'y' or 'n': ");
+            printf("\nWould you like to play another round? Type 'y' for yes or 'n' for no : ");
             scanf(" %c", &playAgain);
+
+            do {
+                if (playAgain == 'y' || playAgain == 'n') {
+                    invalidChoice = 0;
+                    continue;
+                } else {
+                    invalidChoice = 1;
+                    printf("\nIncorrect input!\n");
+                    printf("\nType either 'y' or 'n' : ");
+                    scanf(" %c", &playAgain);
+                }
+            } while (invalidChoice == 1);
             clearScreen();
+
         } else { //implies that the game is over and resets the user's points to 1000
             userPoints = 1000;
-            printf("\nWould you like to play again? Type 'y' or 'n': "); //display a more appropriate message
+            printf("\nWould you like to play again? Type 'y' for yes or 'n' for no : "); //display a more appropriate message
             scanf(" %c", &playAgain);
+            do {
+                if (playAgain == 'y' || playAgain == 'n') {
+                    invalidChoice = 0;
+                    continue;
+                } else {
+                    invalidChoice = 1;
+                    printf("\nIncorrect input!\n");
+                    printf("\nType either 'y' or 'n' : ");
+                    scanf(" %c", &playAgain);
+                }
+            } while (invalidChoice == 1);
+
             clearScreen();
         }
 
     } while (playAgain == 'y' && userPoints > 0);
 
     if (playAgain == 'n') { //only displays if player chooses to exit the game
-        printf("Exiting game... Goodbye!\n");
+        printf("Game Exited... Thank you for playing!\n");
     }
 
     return 0;
